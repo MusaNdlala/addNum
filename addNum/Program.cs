02cfecc8c002ef19eeb16c04b50f17fe2c4a1aa9
Musa <Musa@DESKTOP-N08JB8A>
2021-09-10 14:29:34 +0200

    Add project files.

---
+++ addNum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace addNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine(addnum(5));
+            Console.Read();
+        }
+        static int addnum(int num){
+            int j = 0;
+            int i = 0;
+            int k = num;
+            while (i <= k)
+            {
+                j += num--;
+                i++;
+            }
+            return j;
+        }
+    }
+}
